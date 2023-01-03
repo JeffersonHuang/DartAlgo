572700a34131bf0e3d3eaa71bfb2692733a8e318
huangjianqing <huangjianqing@52tt.com>
2023-01-03 13:45:30 +0800

    添加测试用例

--- test/january_test/january_day03_test.dart
+++ test/january_test/january_day03_test.dart
@@ -4,7 +4,10 @@ import 'package:untitled/january/day03/lc1636.dart' as $lc1636;
 
 void main() {
   test("lc1636 Solution frequencySort", () {
-    expect($lc1636.Solution().frequencySort([1, 1, 2, 2, 2, 3, 4]),
-        [4, 3, 1, 1, 2, 2, 2]);
+    expect($lc1636.Solution().frequencySort([1, 1, 2, 2, 2, 3]),
+        [3, 1, 1, 2, 2, 2]);
+    expect($lc1636.Solution().frequencySort([2, 3, 1, 3, 2]), [1, 3, 3, 2, 2]);
+    expect($lc1636.Solution().frequencySort([-1, 1, -6, 4, 5, -6, 1, 4, 1]),
+        [5, -1, 4, 4, -6, -6, 1, 1, 1]);
   });
 }

02d19ed873275760c4af8802c2e4366e80ec8713
Jefferson <Archkernel@outlook.com>
2023-02-04 16:43:34 +0800

    64. 最小路径和 test

---
+++ test/dp_test/dp_test.dart
@@ -0,0 +1,13 @@
+import 'package:test/expect.dart';
+import 'package:test/scaffolding.dart';
+import 'package:untitled/dp/lc64.dart';
+void main(){
+  test('lc64 最小路径和 1 ', (){
+    final grid = [[1,3,1],[1,5,1],[4,2,1]];
+    expect(Solution().minPathSum(grid), 7);
+  });
+  test('lc64 最小路径和 2', (){
+    final grid = [[1,2,3],[4,5,6]];
+    expect(Solution().minPathSum(grid), 12);
+  });
+}
